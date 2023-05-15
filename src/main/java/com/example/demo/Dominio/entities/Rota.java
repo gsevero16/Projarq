3012adbe4f928dddd82ab09c8f19f3262a126084
ESKieroff <rodrigo.rosa81@edu.pucrs.br>
2023-05-15 19:40:17 -0300

    inicio

--- src/main/java/com/example/demo/Dominio/entities/Rota.java
+++ src/main/java/com/example/demo/Dominio/entities/Rota.java
@@ -1,7 +1,12 @@
 package com.example.demo.Dominio;
 
 public class Rotas {
-    
+    private String nome;
+    private Aeroporto origem;
+    private Aeroporto destino;
+    private double distancia;
+
+
 
 
     public listaRotaLivre(){
