2b6e4c4e88f3e30b82f16a315b1da0fe59189118
ESKieroff <65424464+ESKieroff@users.noreply.github.com>
2023-05-17 17:57:28 -0300

    plano de voo service

--- src/main/java/domain/DTO/PlanoDeVooDTO.java
+++ src/main/java/domain/DTO/PlanoDeVooDTO.java
@@ -1,5 +1,19 @@
 package domain.DTO;
 
+import java.time.LocalDate;
+
 public class PlanoDeVooDTO {
-    
+    public int rotaId;
+    public LocalDate data;
+    public float horarioPartida;
+    public float velocidadeCruzeiro;
+    public int altitude;
+
+    public PlanoDeVooDTO(int rotaId, LocalDate data, float horarioPartida, float velocidadeCruzeiro, int altitude) {
+        this.rotaId = rotaId;
+        this.data = data;
+        this.horarioPartida = horarioPartida;
+        this.velocidadeCruzeiro = velocidadeCruzeiro;
+        this.altitude = altitude;
+    }
 }
