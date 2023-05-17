5f21503119c3e02e41447defa57816ba824e205c
ESKieroff <65424464+ESKieroff@users.noreply.github.com>
2023-05-17 16:58:10 -0300

    plano voo

--- src/main/java/domain/entities/PlanoDeVoo.java
+++ src/main/java/domain/entities/PlanoDeVoo.java
@@ -1,7 +1,41 @@
 package domain.entities;
-public class PlanoDeVoo{
 
-    public autorizaPlanoVoo(){
-        
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="planodevoo")
+public class PlanoDeVoo{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    public int id;
+    public float horarioPartida;
+    public LocalDate data;
+    public float altitude;
+    public float velocidadeCruzeiro;
+
+    @ManyToOne
+    @JoinColumn( name = "id_rota")
+    public Rota rota;
+
+    public PlanoDeVoo() { }
+    //public autorizaPlanoVoo(){ }
+    public PlanoDeVoo(float horarioPartida, LocalDate data, int altitude, float velocidadeCruzeiro, Rota rota) {
+        this.horarioPartida = horarioPartida;
+        this.data = data;
+        this.altitude = altitude;
+        this.velocidadeCruzeiro = velocidadeCruzeiro;
+        this.rota = rota;
     }
+
+
+
 }
