8d0f9a430c6f3234181d35ff6cd51462cdc22149
ESKieroff <65424464+ESKieroff@users.noreply.github.com>
2023-05-17 16:45:53 -0300

    rota

--- src/main/java/domain/entities/Rota.java
+++ src/main/java/domain/entities/Rota.java
@@ -1,15 +1,56 @@
 package domain.entities;
 
+
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="rota")
 public class Rota {
-    private String nome;
+
+/*
+ *     public static final int id = 0;
+    public static final List<Aerovia> aerovias = null;
+    public String nome;
     private Aeroporto origem;
     private Aeroporto destino;
     private double distancia;
+ */
+    //public listaRotaLivre(){}
+@ManyToMany
+ @JoinTable(
+    name = " rota_aerovia ",
+    joinColumns =  @JoinColumn(name = " id_rota "),
+    inverseJoinColumns = @JoinColumn(name = " id_aerovia "))
+    public List<Aerovia> aerovias;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int id;
+    
+    public String nome;
 
+    @ManyToOne
+    @JoinColumn(name = "ref_geo_origin_id")
+    public Aeroporto origem;
 
+    @ManyToOne
+    @JoinColumn(name = "ref_geo_destiny_id")
+    public Aeroporto destino;
 
-    public listaRotaLivre(){
-
+    public Rota(){
+        
     }
+
 }
