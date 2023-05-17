aee39ae5700b5a6fc941cfbbe4321a211339c5d5
ESKieroff <65424464+ESKieroff@users.noreply.github.com>
2023-05-17 16:01:22 -0300

    cansado já

--- src/main/java/domain/entities/OcupacaoAerovia.java
+++ src/main/java/domain/entities/OcupacaoAerovia.java
@@ -2,4 +2,8 @@ package domain.entities;
 
 public class OcupacaoAerovia {
 
+    public static final String data = null;
+    public static final int aeroviaId = 0;
+    public static final float horaSlot = 0;
+
 }
